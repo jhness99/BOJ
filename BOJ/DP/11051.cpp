ddc74fa30c452cdbb361257d3928bc2ff52359ea
Shinogod_ <39560481+jhness99@users.noreply.github.com>
2023-02-28 17:26:16 +0900

    0228

--- BOJ/DP/11051.cpp
+++ BOJ/DP/11051.cpp
@@ -6,8 +6,53 @@
 * 3.변수형
 * int : 10007로 나머지 연산을 하므로 int
 */
+//#include <iostream>
+//
+//using namespace std;
+//
+//int dp[1001][1001];
+//
+//int main(void) {
+//
+//	int N, K;
+//	cin >> N >> K;
+//
+//	dp[0][0] = 1;
+//	dp[1][0] = 1;
+//	dp[1][1] = 1;
+//
+//	for (int i = 2; i <= N; i++) {
+//		for (int j = 0; j <= i; j++) {
+//			if (j == 0) {
+//				dp[i][0] = 1;
+//				continue;
+//			}
+//			dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j])%10007;
+//		}
+//	}
+//	cout << dp[N][K];
+// 
+//	return 0;
+//}
+//2023-02-28
+/*
+* 1.아이디어
+* 파스칼의 삼각형을 풀 듯이 하면 된다. 주어진 값이 5 2 일경우
+* 1
+* 1 1
+* 1 2 1
+* 1 3 3 1
+* 1 4 6 4 1
+* 1 5 10 5 1
+* 이런식의 파스칼삼각형을 dp로 구할 수 있다.
+* dp[i][j] = dp[i-1][j-1] + dp[i-1][j]
+* 초기값 3개만 1로 주어주고 나머지는 점화식으로 가능하다.
+* 2.시간복잡도
+* O(NK) N,K <= 1000 따라서 가능
+* 3.변수형
+*
+*/
 #include <iostream>
-
 using namespace std;
 
 int dp[1001][1001];
@@ -21,16 +66,17 @@ int main(void) {
 	dp[1][0] = 1;
 	dp[1][1] = 1;
 
-	for (int i = 2; i <= N; i++) {
+	for (int i = 0; i <= N; i++) {
 		for (int j = 0; j <= i; j++) {
 			if (j == 0) {
-				dp[i][0] = 1;
+				dp[i][j] = 1;
 				continue;
 			}
-			dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j])%10007;
+			dp[i][j] = (dp[i - 1][j] + dp[i - 1][j - 1]) % 10007;
 		}
 	}
+
 	cout << dp[N][K];
- 
+
 	return 0;
 }
--- BOJ/DP/11054.cpp
+++ BOJ/DP/11054.cpp
@@ -13,40 +13,100 @@
 * 3.변수형
 * int : 
 */
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//
+//using namespace std;
+//
+//int dp[2][1001];
+//
+//int main(void) {
+//
+//	int N;
+//	cin >> N;
+//
+//	vector<int> v(N);
+//	for (auto& i : v)
+//		cin >> i;
+//
+//	for (int i = 0; i < 2; i++) {
+//		for (int j = 0; j < N; j++) {
+//			dp[i][j] = 1;
+//			for (int k = 0; k < j; k++) {
+//				if (v[j] > v[k] && dp[i][j] <= dp[i][k]) {
+//					dp[i][j] = dp[i][k] + 1;
+//				}
+//			}
+//		}
+//		reverse(v.begin(), v.end());
+//	}
+//
+//	int answer = 0;
+//	for (int i = 0; i < N; i++)
+//		answer = max(answer, dp[0][i] + dp[1][N-i-1]);
+//
+//	cout << answer-1;
+//
+//	return 0;
+//}
+//2023-02-28
+/*
+* 1.아이디어
+* 가장 긴 바이토닉 부분수열을 푸는 문제인데, 이 문제는 각각 왼쪽 오른쪽을 기준으로한
+* 가장 긴 증가하는 수열 LIS를 확인해서 서로를 뺀 값 중 최대값이 가장 긴 바이토닉 수열이다.
+*
+* 이 문제는 dp로 풀어보자
+*
+* dp로 풀려면 일단 dp[1001]을 생성한다.
+* dp[1] = 1;
+* i 1->N
+*	dp[i] = 1 이때는 해당 숫자가 있는 수열의 길이가1이라는뜻
+*	j 1->j
+*		if(arr[i] > arr[j] && dp[i] <= dp[j])
+비교중인 i번째수가 j번째보다 크고, j번째 수가 들어간 수열이 i번째 수가 들어간 수열보다 같거나 클경우
+*			dp[i] = dp[j]+1
+*
+* 이것이 dp로 LIS구하는 점화식인데, 이것을 2개 생성해서 서로 빼줌으로써 바이토닉 수열을 만든다.
+* 이때 서로 가장 마지막 부분이 겹치므로 -1을 해줘야한다.
+* 2.시간복잡도
+* 3.변수형
+*/
 #include <iostream>
-#include <vector>
 #include <algorithm>
 
 using namespace std;
 
 int dp[2][1001];
+int arr[1001];
 
 int main(void) {
 
-	int N;
+	int N, answer = 0;;
 	cin >> N;
 
-	vector<int> v(N);
-	for (auto& i : v)
-		cin >> i;
+	for (int i = 0; i < N; i++) {
+		cin >> arr[i];
+	}
+
 
 	for (int i = 0; i < 2; i++) {
 		for (int j = 0; j < N; j++) {
 			dp[i][j] = 1;
 			for (int k = 0; k < j; k++) {
-				if (v[j] > v[k] && dp[i][j] <= dp[i][k]) {
+				if (arr[j] > arr[k] && dp[i][j] <= dp[i][k]) {
 					dp[i][j] = dp[i][k] + 1;
 				}
 			}
 		}
-		reverse(v.begin(), v.end());
+		reverse(arr, arr + N);
 	}
 
-	int answer = 0;
-	for (int i = 0; i < N; i++)
-		answer = max(answer, dp[0][i] + dp[1][N-i-1]);
 
-	cout << answer-1;
+	for (int i = 0; i < N; i++)
+		answer = max(answer, dp[0][i] + dp[1][(N - 1) - i]);
+
+	cout << answer - 1;
 
 	return 0;
 }
