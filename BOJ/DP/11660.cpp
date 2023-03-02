63cf39419a94613778319648565d20f31c8780f0
Shinogod_ <39560481+jhness99@users.noreply.github.com>
2023-03-02 18:06:23 +0900

    0302

--- BOJ/DP/11660.cpp
+++ BOJ/DP/11660.cpp
@@ -14,33 +14,80 @@
 * 3.변수형
 * int : 1024*1024*1000이므로 int
 */
+//#include <iostream>
+//using namespace std;
+//
+//int dp[1025][1025], arr[1025][1025];
+//
+//int main(void) {
+//	//시간초과 나와서 입력쪽 문제인 것으로 판단해 추가
+//	ios_base::sync_with_stdio(false);
+//	cin.tie(NULL);
+//	cout.tie(NULL);
+//
+//	int N, M;
+//	cin >> N >> M;
+//
+//	for (int i = 1; i <= N; i++) {
+//		for (int j = 1; j <= N; j++) {
+//			cin >> arr[i][j];
+//			dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + arr[i][j];
+//		}
+//	}
+//	int answer = 0;
+//	for (int i = 0; i < M; i++) {
+//		int x1, x2, y1, y2;
+//		cin >> x1 >> y1 >> x2 >> y2;
+//
+//		answer = dp[x2][y2] - dp[x2][y1 - 1] - dp[x1 - 1][y2] + dp[x1 - 1][y1 - 1];
+//		cout << answer << "\n";
+//	}
+//
+//	return 0;
+//}
+//2023-03-02 복습
+/*
+* 1.아이디어
+* 행렬을 입력받아서 dp를 초기화 해준다. 이때 dp[i][j]는 (0,0)~(j,i) 까지의 수들의 합이다.
+* dp를 구하기위해선 해당 위치에서 위, 왼쪽의 dp를 더하고 중첩되는 부분을 뺀 후 자신의 값을 더해주면 된다.
+* 이를 점화식으로 표현하면 dp[i][j] = arr[i][j] + dp[i][j-1] + dp[i-1][j] - dp[i-1][j-1] 이다.
+*
+* dp는 완성했고 이제 0,0 부터 시작하는것이 아닌 직접 지정해서 값을 알아내야 한다.
+* (x1, y1)부터 (x2, y2)까지 합은, dp[y2][x2] - dp[y2][x1 - 1] - dp[y1-1][x2] + dp[y1-1][x1-1]
+*
+* 2.시간복잡도
+* 3.변수형
+*
+*/
 #include <iostream>
+
 using namespace std;
 
-int dp[1025][1025], arr[1025][1025];
+int dp[1025][1025];
+int arr[1025][1025];
 
 int main(void) {
-	//시간초과 나와서 입력쪽 문제인 것으로 판단해 추가
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-
 	int N, M;
-	cin >> N >> M;
+	cin >> N >> M;;
+
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++)
+			cin >> arr[i][j];
+	}
 
 	for (int i = 1; i <= N; i++) {
 		for (int j = 1; j <= N; j++) {
-			cin >> arr[i][j];
-			dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + arr[i][j];
+			dp[i][j] = arr[i][j] + dp[i][j - 1] + dp[i - 1][j] - dp[i - 1][j - 1];
 		}
 	}
-	int answer = 0;
-	for (int i = 0; i < M; i++) {
-		int x1, x2, y1, y2;
-		cin >> x1 >> y1 >> x2 >> y2;
 
-		answer = dp[x2][y2] - dp[x2][y1 - 1] - dp[x1 - 1][y2] + dp[x1 - 1][y1 - 1];
-		cout << answer << "\n";
+	for (int i = 0; i < M; i++) {
+		int x1, y1, x2, y2;
+		cin >> x1 >> y1 >> x2 >> y2;
+		cout << dp[x2][y2] - dp[x1 - 1][y2] - dp[x2][y1 - 1] + dp[x1 - 1][y1 - 1] << "\n";
 	}
 
 	return 0;
--- BOJ/DP/11722.cpp
+++ BOJ/DP/11722.cpp
@@ -7,31 +7,82 @@
 * 3.변수형
 * int :
 */
+//#include <iostream>
+//
+//using namespace std;
+//
+//int dp[1001];
+//int arr[1001];
+//
+//int main(void) {
+//
+//	int N;
+//	cin >> N;
+//
+//	for (int i = 1; i <= N; i++)
+//		cin >> arr[i];
+//	
+//	int Max = -1;
+//	for (int i = 1; i <= N; i++) {
+//		dp[i] = 1;
+//		for (int j = 1; j < i; j++) {
+//			if (arr[i] < arr[j] && dp[i] <= dp[j]) {
+//				dp[i] = dp[j] + 1;
+//			}
+//		}
+//		Max = max(Max, dp[i]);	//무슨 이유인지 모르겠지만 아래에 뒀더니 문제해결 반례를 못찾겠음;;
+//	}
+//
+//	cout << Max;
+//
+//	return 0;
+//}
+//2023-03-02 복습
+/*
+* 1.아이디어
+* LIS를 반대로 하면 된다. reverse를 사용해 배열을 반대로 만든 후 LIS를 구해준다.
+*
+* LIS dp로 구하는법
+* dp[i] : i번째 수가 포함된 수열의 최고 길이를 의미한다.
+*
+* 따라서 dp[i]를 갱신해주는데, j 0->i-1 를 해서 만약 arr[i] > arr[j](j번째 수가 더 작고),
+* j번째 수가 포함된 수열이 i번째 포함된 수열보다 같거나 클 경우 우리는 dp[i]를 dp[j]+1로 갱신해준다(j번째 수가 포함된 수열에 i를 추가한다는 뜻)
+*
+* 2.시간복잡도
+* O(N^2), N<=1000 따라서 가능
+* 3.변수형
+*
+*/
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int dp[1001];
-int arr[1001];
+int dp[1000];
 
 int main(void) {
 
 	int N;
 	cin >> N;
 
-	for (int i = 1; i <= N; i++)
-		cin >> arr[i];
-	
-	int Max = -1;
-	for (int i = 1; i <= N; i++) {
+	vector<int> v(N);
+	for (auto& i : v)
+		cin >> i;
+
+	reverse(v.begin(), v.end());
+
+	for (int i = 0; i < N; i++) {
 		dp[i] = 1;
-		for (int j = 1; j < i; j++) {
-			if (arr[i] < arr[j] && dp[i] <= dp[j]) {
+		for (int j = 0; j < i; j++) {
+			if (v[i] > v[j] && dp[j] >= dp[i])
 				dp[i] = dp[j] + 1;
-			}
 		}
-		Max = max(Max, dp[i]);	//무슨 이유인지 모르겠지만 아래에 뒀더니 문제해결 반례를 못찾겠음;;
 	}
+
+	int Max = -1;
+	for (int i = 0; i < N; i++)
+		Max = max(Max, dp[i]);
 
 	cout << Max;
 
