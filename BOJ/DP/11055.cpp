3d473005a304db474593c0e18dfd00dd6edbcda9
Shinogod_ <39560481+jhness99@users.noreply.github.com>
2023-03-02 14:02:00 +0900

    0301

--- BOJ/DP/11055.cpp
+++ BOJ/DP/11055.cpp
@@ -16,33 +16,84 @@
 * 3.변수형
 * int : 
 */
+//#include <iostream>
+//#include <vector>
+//
+//using namespace std;
+//
+//long long dp[1001];
+//
+//int main(void) {
+//
+//	int N;
+//	cin >> N;
+//
+//	vector<int> v(N);
+//	for (auto& i : v)
+//		cin >> i;
+//
+//	for (int i = 0; i < N; i++) {
+//		dp[i] = v[i];
+//		for (int j = 0; j < i; j++)
+//			if (v[i] > v[j] && dp[i] < dp[j] + v[i])
+//				dp[i] = dp[j] + v[j];
+//
+//	}
+//
+//	long long Max = -1;
+//	for (int i = 0; i < N; i++)
+//		Max = max(Max, dp[i]);
+//
+//	cout << Max;
+//
+//	return 0;
+//}
+//2023-03-01 복습
+/*
+* 1.아이디어
+* dp[i] : i번째 수가 포함된 증가 부분 수열의 수들의 합
+* 가장 큰 증가 부분 수열을 구하는 것처럼 하는데 길이가 아닌 수열의 수들의 총 합을 출력한다
+* 
+*  dp[i] < dp[j] + v[i]부분에서 또 햇갈렷지만 다시 이해 완료 없을경우 반례 1 8 2 3 9
+* 
+* for(int i = 1; i <= N; i++){
+*	dp[i] = arr[i];
+*	for(int j = 1; j < i;j++){
+*		if(arr[i] > arr[j] && dp[i] < dp[j] + v[i])
+*			dp[i] = dp[j] + arr[i];
+* }
+* 2.시간복잡도
+* O(N^2) N<=1000 이므로 가능
+* 3.변수형
+* 
+*/
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
-long long dp[1001];
+int dp[1001];
+int arr[1001];
 
 int main(void) {
 
 	int N;
 	cin >> N;
 
-	vector<int> v(N);
-	for (auto& i : v)
-		cin >> i;
+	for (int i = 1; i <= N; i++)
+		cin >> arr[i];
 
-	for (int i = 0; i < N; i++) {
-		dp[i] = v[i];
-		for (int j = 0; j < i; j++)
-			if (v[i] > v[j] && dp[i] < dp[j] + v[i])
-				dp[i] += v[j];
-
+	for (int i = 1; i <= N; i++) {
+		dp[i] = arr[i];
+		for (int j = 1; j < i; j++) {
+			if (arr[i] > arr[j] && dp[i] < dp[j] + arr[i])
+				dp[i] = dp[j] + arr[i];
+		}
 	}
 
-	long long Max = -1;
-	for (int i = 0; i < N; i++)
+	int Max = -1;
+	for (int i = 1; i <= N; i++) {
 		Max = max(Max, dp[i]);
+	}
 
 	cout << Max;
 
--- BOJ/DP/1699.cpp
+++ BOJ/DP/1699.cpp
@@ -15,21 +15,56 @@
 * 3.변수형
 * int :
 */
+//#include <iostream>
+//using namespace std;
+//
+//int dp[100001];
+//
+//int main(void) {
+//
+//	int N;
+//	cin >> N;
+//
+//	for (int i = 1; i <= N; i++) {
+//		dp[i] = i;
+//		for (int j = 0; j * j <= i; j++) {
+//			dp[i] = min(dp[i], dp[i - j * j] + 1);
+//		} 
+//	}
+//
+//	cout << dp[N];
+//
+//	return 0;
+//}
+/*
+* 1.아이디어
+* dp[i] i를 구성하는 제곱항의 개수
+*
+* 1,2,3읜 각각 개수가 1,2,3이다.
+* 여기서 4부터는 2의 제곱이 있기때문에 달라지는데 이를 확인하는 것은
+* dp[i] = max(dp[i], dp[i-j*j]+1)
+*
+* 2.시간복잡도
+* 3.변수형
+*/
 #include <iostream>
+
 using namespace std;
 
 int dp[100001];
 
 int main(void) {
-
 	int N;
 	cin >> N;
 
-	for (int i = 1; i <= N; i++) {
-		dp[i] = i;
-		for (int j = 0; j * j <= i; j++) {
+	dp[1] = 1;
+	dp[2] = 2;
+	dp[3] = 3;
+
+	for (int i = 4; i <= N; i++) {
+		dp[i] = i;		//모든 항을 1로 했을때, 즉 자기자신을 기본값으로 두고 가장 작은수로 갱신하도록 하게 함
+		for (int j = 1; j * j <= i; j++)
 			dp[i] = min(dp[i], dp[i - j * j] + 1);
-		} 
 	}
 
 	cout << dp[N];
