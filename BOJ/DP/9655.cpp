bb2d4db3754183b9ab5abbdc2550c98cfca256dd
Shinogod_ <39560481+jhness99@users.noreply.github.com>
2023-03-07 17:21:41 +0900

    0307

--- BOJ/DP/9655.cpp
+++ BOJ/DP/9655.cpp
@@ -5,8 +5,29 @@
 * 3.변수형
 * 
 */
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main(void) {
+//
+//	int N;
+//	cin >> N;
+//
+//	if (N % 2 == 0)
+//		cout << "CY";
+//	else
+//		cout << "SK";
+//
+//	return 0;
+//}
+//2023-03-07 복습
+/*
+* 1.아이디어
+* 1과 3을 가져갈 수 있고 SK와 CY가 게임을 할 때 홀수일경우 무조건 SK가 이기게 되고 짝수이면 CY가 무조건 이기게 된다.
+*
+*/
 #include <iostream>
-
 using namespace std;
 
 int main(void) {
@@ -14,10 +35,7 @@ int main(void) {
 	int N;
 	cin >> N;
 
-	if (N % 2 == 0)
-		cout << "CY";
-	else
-		cout << "SK";
+	cout << ((N % 2 == 0) ? "CY" : "SK");
 
 	return 0;
 }
