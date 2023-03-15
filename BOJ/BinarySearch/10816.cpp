368dbc2ae3266fd2ec2e6b13dc974e1143b31a53
Shinogod_ <39560481+jhness99@users.noreply.github.com>
2023-03-15 20:35:04 +0900

    0315

--- BOJ/BinarySearch/10816.cpp
+++ BOJ/BinarySearch/10816.cpp
@@ -9,58 +9,102 @@
 * 해시맵의 시간복잡도는 O(1), N<=500000, M<= 500000
 * 3.변수형
 */
+//#include <iostream>
+//#include <algorithm>
+//#include <unordered_map>
+//#include <vector>
+//
+//using namespace std;
+//
+//int main(void) {
+//	ios::sync_with_stdio(false);
+//	cin.tie(0);
+//	int N, M;
+//	int temp;
+//	cin >> N;
+//
+//	vector<int> v(N);
+//
+//	for (auto& i : v)
+//		cin >> i;
+//	
+//	sort(v.begin(), v.end());
+//
+//	cin >> M;
+//
+//	for (int i = 0; i < M; i++) {
+//		int temp;
+//		cin >> temp;
+//
+//		cout << upper_bound(v.begin(), v.end(), temp) - lower_bound(v.begin(), v.end(), temp) << " ";
+//	}
+//
+//	/*
+//	unordered_map<int, int> hm;
+//
+//	for (int i = 0; i < N; i++) {
+//
+//		cin >> temp;
+//		if (hm.find(temp) != hm.end())
+//			hm[temp]++;
+//		else
+//			hm.insert({ temp, 1 });
+//	}
+//
+//	cin >> M;
+//
+//	for (int i = 0; i < M; i++) {
+//		cin >> temp;
+//		if (hm.find(temp) != hm.end())
+//			cout << hm[temp];
+//		else
+//			cout << 0;
+//		cout << " ";
+//	}*/
+//
+//	return 0;
+//}
+//2023-03-15 복습
+/*
+* 1.아이디어
+* 이 문제는 해시맵으로 풀어도 되지만 이분탐색으로 풀어보자
+*
+* 아주 쉽다. 첫번째를 정렬하고 uppor_bound - lower_bound 하면 되는것이다.
+* 그러면 마지막 위치 + 1 -  처음 숫자를 발견한 위치 하면 해당 갯수가 나온다.
+* 예를들어 1 2 3 4 5 5 5 의 경우 7 - 4 = 3으로 5의 개수가 나온다.
+*
+* 2.시간복잡도
+* N개의 수에서 이분탐색을 하므로 O(logN), 이분탐색을 M번 하므로 O(MlogN), M N<=500000
+* 3.변수형
+*
+*/
 #include <iostream>
-#include <algorithm>
-#include <unordered_map>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main(void) {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
 	int N, M;
-	int temp;
 	cin >> N;
 
-	vector<int> v(N);
-
-	for (auto& i : v)
+	vector<int> A(N);
+	for (auto& i : A)
 		cin >> i;
-	
-	sort(v.begin(), v.end());
+
+	sort(A.begin(), A.end());
 
 	cin >> M;
+	while (M--) {
+		int target;
+		cin >> target;
 
-	for (int i = 0; i < M; i++) {
-		int temp;
-		cin >> temp;
-
-		cout << upper_bound(v.begin(), v.end(), temp) - lower_bound(v.begin(), v.end(), temp) << " ";
+		cout << (upper_bound(A.begin(), A.end(), target) - A.begin()) - (lower_bound(A.begin(), A.end(), target) - A.begin()) << " ";
 	}
-
-	/*
-	unordered_map<int, int> hm;
-
-	for (int i = 0; i < N; i++) {
-
-		cin >> temp;
-		if (hm.find(temp) != hm.end())
-			hm[temp]++;
-		else
-			hm.insert({ temp, 1 });
-	}
-
-	cin >> M;
-
-	for (int i = 0; i < M; i++) {
-		cin >> temp;
-		if (hm.find(temp) != hm.end())
-			cout << hm[temp];
-		else
-			cout << 0;
-		cout << " ";
-	}*/
 
 	return 0;
 }
--- BOJ/BinarySearch/1654.cpp
+++ BOJ/BinarySearch/1654.cpp
@@ -15,42 +15,96 @@
 * long long total : 만약 mid가 엄청 작은수가 나오고 최대값을 나눈 몫이 커지게 되면 int를 넘게 되므로 long long
 * 나머지는 int:
 */
+//#include <iostream>
+//#include <vector>
+//
+//using namespace std;
+//
+//int main(void) {
+//	std::ios::sync_with_stdio(false);
+//	std::cin.tie(NULL);
+//
+//	int K, N;
+//	cin >> K >> N;
+//
+//	long long left = 1;
+//	long long right = -1;
+//	int answer = 0;
+//
+//	vector<long long> LAN(K);
+//	for (auto& i : LAN) {
+//		cin >> i;
+//		right = max(i, right);
+//	}
+//
+//	while (left <= right) {
+//		long long total = 0;
+//		long long mid = (left + right) / 2;
+//		for (int i = 0; i < K; i++) 
+//			total += (LAN[i] / mid);
+//
+//		if (total < N)
+//			right = mid - 1;
+//		else {
+//			left = mid + 1;
+//			answer = max(answer, mid);
+//		}
+//	}
+//	
+//	cout << answer;
+//
+//	return 0;
+//}
+//2023-03-15 복습
+/*
+* 1.아이디어
+* 가장 길게 랜선을 잘라야 하므로 완전탐색을 사용하면 2^31-1를 일일이 확인해야 하는데 이러면 시간초과가 걸리게 된다.
+* 따라서 이분탐색으로 접근한다.
+*
+* left = 1, right = 가장 큰 값을 기준으로 mid를 만들어서 이분탐색을 한다.
+* 각 랜선을 mid로 나눠서 나온 값을 total에 더해주고 total이 만약 N과 같거나 크다면, left = mid + 1, answer = mid;
+* 만약 total이 N보다 작다면 right = mid - 1 을 한다.
+* 2.시간복잡도
+* log2^31-1를 K번 반복하므로 O(Klog2^32-1), K<=10000, 따라서 가능
+* 3.변수형
+* long long total : mid가 1일경우, 10000*1000000이 최대값이 되므로 long long
+*/
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
 int main(void) {
-	std::ios::sync_with_stdio(false);
-	std::cin.tie(NULL);
 
-	int K, N;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int K, N, answer = 0;
 	cin >> K >> N;
 
-	long long left = 1;
-	long long right = -1;
-	int answer = 0;
-
-	vector<long long> LAN(K);
+	int Max = -1;
+	vector<int> LAN(K);
 	for (auto& i : LAN) {
 		cin >> i;
-		right = max(i, right);
+		Max = max(Max, i);
 	}
+
+	long long left = 1;
+	long long right = Max;
 
 	while (left <= right) {
-		long long total = 0;
 		long long mid = (left + right) / 2;
-		for (int i = 0; i < K; i++) 
-			total += (LAN[i] / mid);
-
-		if (total < N)
-			right = mid - 1;
-		else {
-			left = mid + 1;
-			answer = max(answer, mid);
+		long long total = 0;
+		for (int i = 0; i < K; i++) {
+			total += LAN[i] / mid;
 		}
+		if (total >= N) {
+			left = mid + 1;
+			answer = mid;
+		}
+		else
+			right = mid - 1;
 	}
-	
 	cout << answer;
 
 	return 0;
