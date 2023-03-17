d38cc7452c55f4fcc3ab26f0232264cb6a65bafd
Shinogod_ <39560481+jhness99@users.noreply.github.com>
2023-03-17 20:48:39 +0900

    0317

--- BOJ/BinarySearch/12015.cpp
+++ BOJ/BinarySearch/12015.cpp
@@ -12,42 +12,102 @@
 * 이분탐색의 시간복잡도 O(logN) * O(N) = O(NlogN), N<=1000000 따라서 가능
 * 3.변수형
 */
+//#include <iostream>
+//#include <vector>
+//
+//using namespace std;
+//
+//int main(void) {
+//	ios_base::sync_with_stdio(false); cin.tie(NULL);;
+//	int N, cnt = 1;
+//	cin >> N;
+//
+//	vector<int> input(N), LIS;
+//	
+//	for (auto& i : input)
+//		cin >> i;
+//
+//	LIS.push_back(input[0]);
+//	for (int i = 1; i < N; i++) {
+//		if (LIS.back() < input[i]) {
+//			LIS.push_back(input[i]);
+//			cnt++;
+//		}
+//		else {
+//			int start = 0;
+//			int end = LIS.size() - 1;
+//			int mid = end;
+//			while (start < end) {
+//				mid = (start + end) / 2;
+//				if (LIS[mid] < input[i])
+//					start = mid + 1;
+//				else
+//					end = mid;
+//			}
+//			LIS[mid] = input[i];
+//		}
+//	}
+//	cout << cnt;
+//
+//	return 0;
+//}
+//2023-03-17 복습
+/*
+* 1.아이디어
+* LIS문제인데 수열의 크기가 100만이므로 dp로 구현하기는 불가능. 따라서 이분탐색으로 접근한다.
+*
+* LIS 이분탐색으로 구현
+* LIS 백터를 만들어 해당 백터를 갱신하면서 최대길이를 찾을 것이다.
+* 먼저 가장 첫번째 수를 LIS 백터에 push_back을 한다.
+* 다음 수부터 확인하면서 LIS백터의 back()보다 크다면, push_back 작다면 이분탐색으로 위치를 찾아 LIS를 갱신해준다.
+*
+* 우리가 찾아야 하는 것은 확인하는 수와 같거나 큰 수중 가장 먼저나오는 수다. 해당 수는 lower_bound로도 찾을 수 있다.
+* 해당 수를 찾는 이분탐색은 left = 0, right = LIS.size()로 설정하고 mid를 만든다.
+* LIS[mid]의 값이 내가 확인하는 값과 같거나 큰 경우, right = mid - 1;
+* LIS[mid]의 값이 내가 확인하는 값보다 작을경우, left = mid + 1, index = mid
+* LIS[index] = arr[i]로 갱신해주면 된다.
+*
+* 2.시간복잡도
+* 이분탐색을 사용했으므로 O(logN) N번 반복하므로 O(NlogN) 따라서 가능
+* 3.변수형
+*/
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
 int main(void) {
-	ios_base::sync_with_stdio(false); cin.tie(NULL);;
-	int N, cnt = 1;
-	cin >> N;
 
-	vector<int> input(N), LIS;
-	
+	int N;
+	cin >> N;
+	vector<int> LIS;
+	vector<int> input(N);
 	for (auto& i : input)
 		cin >> i;
 
 	LIS.push_back(input[0]);
 	for (int i = 1; i < N; i++) {
-		if (LIS.back() < input[i]) {
+		if (LIS.back() < input[i])
 			LIS.push_back(input[i]);
-			cnt++;
-		}
 		else {
-			int start = 0;
-			int end = LIS.size() - 1;
-			int mid = end;
-			while (start < end) {
-				mid = (start + end) / 2;
-				if (LIS[mid] < input[i])
-					start = mid + 1;
-				else
-					end = mid;
+			int left = 0;
+			int right = LIS.size() - 1;
+			int index = 0;
+			while (left <= right) {
+				int mid = (left + right) / 2;
+				if (LIS[mid] < input[i]) {
+					left = mid + 1;
+				}
+				else {
+					index = mid;
+					right = mid - 1;
+				}
 			}
-			LIS[mid] = input[i];
+			LIS[index] = input[i];
 		}
 	}
-	cout << cnt;
+
+	cout << LIS.size();
 
 	return 0;
 }
--- BOJ/BinarySearch/2512.cpp
+++ BOJ/BinarySearch/2512.cpp
@@ -12,6 +12,60 @@
 * O(NlogM), N<=10000,M<=1000000000 이므로 가능
 * 3.변수형
 */
+//#include <iostream>
+//#include <vector>
+//
+//using namespace std;
+//
+//int main(void) {
+//
+//	int N, M, Max = -1, answer = 0;
+//	cin >> N;
+//
+//	vector<int> v(N);
+//	for (auto& i : v) {
+//		cin >> i;
+//		Max = max(Max, i);
+//	}
+//		
+//	cin >> M;
+//
+//	int start = 1;
+//	int end = Max;
+//	while (start <= end) {
+//		int mid = (start + end) / 2;
+//		int total = 0;
+//		for (int i = 0; i < N; i++) {
+//			if (v[i] > mid)
+//				total += mid;
+//			else
+//				total += v[i];
+//		}
+//		if (total > M)
+//			end = mid - 1;
+//		else {
+//			start = mid + 1;
+//			answer = mid;
+//		}
+//	}
+//	cout << answer;
+//
+//	return 0;
+//}
+//2023-03-17 복습
+/*
+* 1.아이디어
+* 단순계산으로 하려면 총 예산값을 기준으로 지방수만큼 반복해야하므로 O(NM) N<=10000,M<=1000000000 이므로 불가능
+* 이분탐색으로 접근한다.
+* 입력값중 최대값을 right 1을 left로 해서 이분탐색을 한다.
+* 이때 mid를 구하고 mid값보다 필요한 예산이 더 적으면, 해당 예산을 전부 더하고
+* 만약 mid보다 크다면, mid를 total에 더해준다.
+* 나온 total과 M을 비교해서, total이 M보다 크댜면, end = mid - 1,
+* total이 M과 같거나 작다면, start = mid + 1; answer = mid;
+* 2.시간복잡도
+* 이분탐색을 사용하면 O(logMax), 해당 이분탐색 안에서 N번만큼 반복하므로, O(NlogMax) N<=10000, Max <=100000 따라서 가능
+* 3.변수형
+*/
 #include <iostream>
 #include <vector>
 
@@ -19,35 +73,41 @@ using namespace std;
 
 int main(void) {
 
-	int N, M, Max = -1, answer = 0;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int N, M, answer = 0;
 	cin >> N;
 
-	vector<int> v(N);
-	for (auto& i : v) {
+	int Max = -1;
+	vector<int> input(N);
+	for (auto& i : input) {
 		cin >> i;
 		Max = max(Max, i);
 	}
-		
+
 	cin >> M;
 
-	int start = 1;
-	int end = Max;
-	while (start <= end) {
-		int mid = (start + end) / 2;
+	int left = 1;
+	int right = Max;
+
+	while (left <= right) {
+		int mid = (left + right) / 2;
 		int total = 0;
 		for (int i = 0; i < N; i++) {
-			if (v[i] > mid)
+			if (input[i] > mid)
 				total += mid;
 			else
-				total += v[i];
+				total += input[i];
 		}
 		if (total > M)
-			end = mid - 1;
+			right = mid - 1;
 		else {
-			start = mid + 1;
+			left = mid + 1;
 			answer = mid;
 		}
 	}
+
 	cout << answer;
 
 	return 0;
