a154a00a168e3c09908dd938bccdbdd73c1dd12a
Shinogod_ <39560481+jhness99@users.noreply.github.com>
2023-03-25 19:27:52 +0900

    0325

--- BOJ/BinarySearch/2473.cpp
+++ BOJ/BinarySearch/2473.cpp
@@ -13,6 +13,71 @@
 * 3.변수형
 * long long : 최대 -10억~10억까지 가능하므로 최대 30억까지 가능, 따라서 long long
 */
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//
+//using namespace std;
+//
+//int main(void) {
+//
+//	int N;
+//	cin >> N;
+//
+//	vector<long long> input(N);
+//	for (auto& i : input)
+//		cin >> i;
+//
+//	int a = 0, b = 0, c = 0;
+//	long long min = 1e10;
+//
+//	sort(input.begin(), input.end());
+//
+//	for (int i = 0; i < N; i++) {
+//		int left = 0;
+//		int right = N - 1;
+//
+//		if (i == 0) left = 1;
+//		if (i == N - 1) right = N - 2;
+//
+//		while (left < right) {
+//			long long temp = input[left] + input[right] + input[i];
+//			if (min > abs(temp)) {
+//				min = abs(temp);
+//				a = input[left];
+//				b = input[i];
+//				c = input[right];
+//			}
+//			if (temp < 0) {
+//				left++;
+//				if (left == i) left++;
+//			}
+//			else {
+//				right--;
+//				if (right == i) right--;
+//			}
+//		}
+//	}
+//	long long arr[3] = { a,b,c };
+//	sort(arr, arr + 3);
+//	for (auto& ll : arr)
+//		cout << ll << " ";
+//
+//	return 0;
+//}
+//2023-03-25 복습
+/*
+* 1.아이디어
+* 두 용액과 비슷한 문제지만 여기서는 3개를 골라야 한다.
+* 따라서 1개는 고정해두고 양쪽에서 확인하는 것을 N번 해주면 된다.
+* O(N^2)되지만 가능하다
+*
+* 투포인터로 양쪽에서 이동하면서 두 값의 합이 양수일경우, right--, 음수일경우 left++를 해준다.
+* 만약 기존값과 비교해서 0과 더 가깝다면,(절대값이 더 작다면) 해당 위치로 갱신해준다.
+* 2.시간복잡도
+* 3.변수형
+*
+*/
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -28,25 +93,26 @@ int main(void) {
 	for (auto& i : input)
 		cin >> i;
 
-	int a = 0, b = 0, c = 0;
-	long long min = 1e10;
-
 	sort(input.begin(), input.end());
-
+	long long  num = 3000000001;
+	int leftNum;
+	int rightNum;
+	int midNum;
 	for (int i = 0; i < N; i++) {
 		int left = 0;
 		int right = N - 1;
-
-		if (i == 0) left = 1;
-		if (i == N - 1) right = N - 2;
+		if (left == i)
+			left++;
+		if (right == i)
+			right--;
 
 		while (left < right) {
-			long long temp = input[left] + input[right] + input[i];
-			if (min > abs(temp)) {
-				min = abs(temp);
-				a = input[left];
-				b = input[i];
-				c = input[right];
+			long long temp = input[right] + input[left] + input[i];
+			if (abs(num) > abs(temp)) {
+				num = temp;
+				leftNum = input[left];
+				rightNum = input[right];
+				midNum = input[i];
 			}
 			if (temp < 0) {
 				left++;
@@ -58,9 +124,10 @@ int main(void) {
 			}
 		}
 	}
-	long long arr[3] = { a,b,c };
-	sort(arr, arr + 3);
-	for (auto& ll : arr)
+
+	int answer[] = { rightNum, leftNum, midNum };
+	sort(answer, answer + 3);
+	for (auto& ll : answer)
 		cout << ll << " ";
 
 	return 0;
--- BOJ/BinarySearch/7453.cpp
+++ BOJ/BinarySearch/7453.cpp
@@ -13,6 +13,65 @@
 * 3.변수형
 * long long : 최대 나올 수 있는 경우의 수는 N^4 따라서 long long 
 */
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//
+//using namespace std;
+//
+//int main(void) {
+//
+//	int N, answer = 0;
+//	vector<int> A;
+//	vector<int> B;
+//
+//	cin >> N;
+//
+//	vector<vector<int>> arr(4, vector<int>(N));
+//	for (int i = 0; i < N; i++) {
+//		for (int j = 0; j < 4; j++)
+//			cin >> arr[j][i];
+//	}
+//
+//	for (int i = 0; i < N; i++) {
+//		for (int j = 0; j < N; j++) {
+//			A.push_back(arr[0][i] + arr[1][j]);
+//			B.push_back(arr[2][i] + arr[3][j]);
+//		}
+//	}
+//	sort(A.begin(), A.end());
+//	sort(B.begin(), B.end());
+//
+//	for (int i = 0; i < A.size(); i++) {
+//		int low = lower_bound(B.begin(), B.end(), -A[i]) - B.begin();
+//		int high = upper_bound(B.begin(), B.end(), -A[i]) - B.begin();
+//
+//		answer += high - low;
+//	}
+//
+//	cout << answer;
+//
+//	return 0;
+//}
+//2023-03-25 복습
+/*
+* 1.아이디어
+* 일단 각 배열의 크기가 4000개이다. 모든 배열의 조합을 계산하면 4000^4가 되기때문에
+* 완전탐색으로는 불가능하다. 따라서 이분탐색을 사용한다.
+*
+* 이분탐색을 사용하기 위해 A와 B, C와 D의 합을 새로 저장해준다.
+* 저장한 2개의 배열의 합의 백터중 1개를 정렬하고
+* 정렬한 것으로 lower_bound, upper_bound를 사용해 -AB[i]가 되는 수의 개수를 구한다.
+* 구한 개수를 total에 저장해준다.
+*
+* 2.시간복잡도
+* 우선 배열을만드는 시간복잡도는 O(2*N^2), 하나를 정렬하는 것은 O(N^2logN^2),
+* 이분탐색을 하는 시간복잡도는 O(N^2logN^2) 따라서 시간복잡도는 O(2*N^2 + 2(N^2logN^2)) 이다.
+* N<=4000 따라서 가능
+*
+* 3.변수형
+* 정수의 절대값이 2^28이므로 모든수는 int로 가능하다. 다만 answer는 모든 경우의 수가 4000^4 이므로 long long으로 해준다.
+*/
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -21,34 +80,40 @@ using namespace std;
 
 int main(void) {
 
-	int N, answer = 0;
-	vector<int> A;
-	vector<int> B;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
 
+	int N;
+	long long answer = 0;
 	cin >> N;
 
-	vector<vector<int>> arr(4, vector<int>(N));
+	vector<int> A(N);
+	vector<int> B(N);
+	vector<int> C(N);
+	vector<int> D(N);
+
+	vector<int> AB;
+	vector<int> CD;
+
 	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < 4; j++)
-			cin >> arr[j][i];
+		cin >> A[i] >> B[i] >> C[i] >> D[i];
 	}
 
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++) {
-			A.push_back(arr[0][i] + arr[1][j]);
-			B.push_back(arr[2][i] + arr[3][j]);
+			AB.push_back(A[i] + B[j]);
+			CD.push_back(C[i] + D[j]);
 		}
 	}
-	sort(A.begin(), A.end());
-	sort(B.begin(), B.end());
 
-	for (int i = 0; i < A.size(); i++) {
-		int low = lower_bound(B.begin(), B.end(), -A[i]) - B.begin();
-		int high = upper_bound(B.begin(), B.end(), -A[i]) - B.begin();
+	sort(CD.begin(), CD.end());
 
-		answer += high - low;
+	for (int i = 0; i < AB.size(); i++) {
+		int lower = lower_bound(CD.begin(), CD.end(), -AB[i]) - CD.begin();
+		int upper = upper_bound(CD.begin(), CD.end(), -AB[i]) - CD.begin();
+
+		answer += upper - lower;
 	}
-
 	cout << answer;
 
 	return 0;
