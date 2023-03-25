9da931d5075a3defd6d079ab735281f36cad7882
Shinogod_ <39560481+jhness99@users.noreply.github.com>
2023-03-25 19:28:17 +0900

    0324

--- BOJ/BinarySearch/2776.cpp
+++ BOJ/BinarySearch/2776.cpp
@@ -8,6 +8,52 @@
 * M회만큼 N크기의 수를 확인하므로, O(MlogN), M, N <=1000000 따라서 가능
 * 3.변수형
 */
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//
+//using namespace std;
+//
+//int main(void) {
+//	ios_base::sync_with_stdio(false);
+//	cin.tie(NULL);
+//
+//	int T;
+//	cin >> T;
+//
+//	while (T--) {
+//		int N, M;
+//		cin >> N;
+//
+//		vector<int> v1(N);
+//		for (auto& i : v1)
+//			cin >> i;
+//
+//		sort(v1.begin(), v1.end());
+//		
+//		cin >> M;
+//		for (int i = 0; i < M; i++) {
+//			int Num;
+//			cin >> Num;
+//			auto upper = upper_bound(v1.begin(), v1.end(), Num);
+//			auto lower = lower_bound(v1.begin(), v1.end(), Num);
+//
+//			if (upper - lower > 0) cout << 1 << '\n';
+//			else cout << 0 << '\n';
+//		}
+//	}
+//
+//	return 0;
+//}
+//2023-03-24 복습
+/*
+* 1.아이디어
+* upper_bound와 lower_bound를 사용해 두 index가 같지 않을경우 해당수가 존재하므로 1, 아닐경우 0을 출력
+*
+* 2.시간복잡도
+* 정렬를 사용했으므로 O(NlogN), 이분탐색을 M번 반복했으므로, O(MlogN) N,M<=1000000 따라서 가능
+* 3.변수형
+*/
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -15,31 +61,37 @@
 using namespace std;
 
 int main(void) {
+
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 
 	int T;
 	cin >> T;
-
 	while (T--) {
 		int N, M;
 		cin >> N;
 
-		vector<int> v1(N);
-		for (auto& i : v1)
+		vector<int> A(N);
+		for (auto& i : A)
 			cin >> i;
 
-		sort(v1.begin(), v1.end());
-		
-		cin >> M;
-		for (int i = 0; i < M; i++) {
-			int Num;
-			cin >> Num;
-			auto upper = upper_bound(v1.begin(), v1.end(), Num);
-			auto lower = lower_bound(v1.begin(), v1.end(), Num);
+		sort(A.begin(), A.end());
 
-			if (upper - lower > 0) cout << 1 << '\n';
-			else cout << 0 << '\n';
+		cin >> M;
+		vector<int> B(M);
+		for (auto& i : B)
+			cin >> i;
+
+		for (int i = 0; i < M; i++) {
+			auto low = lower_bound(A.begin(), A.end(), B[i]);
+			auto upper = upper_bound(A.begin(), A.end(), B[i]);
+
+			if (upper != low)
+				cout << 1;
+			else
+				cout << 0;
+
+			cout << "\n";
 		}
 	}
 
--- BOJ/BinarySearch/3020.cpp
+++ BOJ/BinarySearch/3020.cpp
@@ -18,6 +18,78 @@
 * 정렬로 인한 복잡도 O(NlogN), 이분탐색을 H번 반복하므로 O(HlogN) 
 * 3.변수형
 */
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//
+//using namespace std;
+//
+//int main(void) {
+//
+//	int N, H;
+//	cin >> N >> H;
+//
+//	vector<int> Top(N / 2);
+//	vector<int> Bottom(N / 2);
+//
+//	for (int i = 0; i < N / 2; i++) {
+//		cin >> Bottom[i] >> Top[i];
+//	}
+//
+//	sort(Top.begin(), Top.end());
+//	sort(Bottom.begin(), Bottom.end());
+//
+//	int answer = 200001;
+//	int num = 0;
+//	for (int hight = 1; hight <= H; hight++) {
+//		int cnt = 0;
+//
+//		int index = lower_bound(Bottom.begin(), Bottom.end(), hight) - Bottom.begin();
+//		cnt += N / 2 - index;
+//
+//		index = upper_bound(Top.begin(), Top.end(), H - hight) - Top.begin();
+//		cnt += N / 2 - index;
+//
+//		
+//		if (answer > cnt) {
+//			answer = cnt;
+//			num = 1;
+//		}else if (answer == cnt) num++;
+//	}
+//
+//	cout << answer << " " << num;
+//
+//	return 0;
+//}
+//2023-03-24 복습
+/*
+* 1.아이디어
+* 이 문제는 석순과 종유석을 각각 해당 높이에서 몇개나 부셔야 하는지 따로 계산해주면된다.
+* 계산한 갯수가 가장 적어진 경우의 높이와 갯수를 출력하면 되는 문제이다.
+*
+* 이제 각 석순과 종유석을 기준으로 몇개나 부셔야 하는지를 구해야 한다.
+* 구하기 위해서 우선 정렬한다.
+* 우선 석순의 경우 정렬했을 때 해당 높이를 기준으로 같거나 큰 석순의 갯수를 구해야 한다.
+* 따라서 lowe_bound를 사용해 해당 높이보다 긴 석순을 구한다. 석순의 총 개수 - index
+* 이렇게 되면 index는 같거나 큰 즉 관통해야하는 석순의 첫번째 index를 가져오므로
+* 해당 높이일 때 부셔야 하는 석순의 개수를 알 수 있다
+*
+* 다음 종유석의 경우는 일단 종유석은 천장에 붙어있으므로 높이를 반대로 생각해줘야 한다.
+* 만약 높이가 1이라면, 부술수 없는 종유석의 위치는 1~천장이 높이 - 1이 된다.
+* 그말은 즉 (천장의 높이-확인하는 높이)의 upper_bound가 첫번째로 부수기 시작하는 위치이고,
+* 해당 값을 종유석의 개수에 빼주면 부셔야 하는 종유석의 갯수가 나오게 된다.
+*
+* 해당 방법으로 모든 관통하는 종유석, 석순의 합을 구하고 해당 합이 전에 구한 합보다 작다면,
+* 갱신해주고 해당 개수와 같은 높이가 있다면 cnt해준다.
+*
+* 2.시간복잡도
+* 최대 N/2개의 종유석과 석순이 있으므로 정렬로 인해 각각 O(2*(N/2)log(N/2)),
+* 이분탐색으로 하는 부분은 O(HlogN/2) 이다. 따라서 O(HlogN)이라고 할 수 있고, N<=200000, H<=500000
+* 따라서 가능
+*
+* 3.변수형
+*
+*/
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -26,38 +98,40 @@ using namespace std;
 
 int main(void) {
 
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
 	int N, H;
 	cin >> N >> H;
 
+	vector<int> Buttom(N / 2);
 	vector<int> Top(N / 2);
-	vector<int> Bottom(N / 2);
 
 	for (int i = 0; i < N / 2; i++) {
-		cin >> Bottom[i] >> Top[i];
+		cin >> Buttom[i] >> Top[i];
 	}
 
+	sort(Buttom.begin(), Buttom.end());
 	sort(Top.begin(), Top.end());
-	sort(Bottom.begin(), Bottom.end());
 
 	int answer = 200001;
-	int num = 0;
-	for (int hight = 1; hight <= H; hight++) {
-		int cnt = 0;
+	int cnt = 0;
+	for (int Height = 1; Height <= H; Height++) {
+		int total = 0;
+		int index = lower_bound(Buttom.begin(), Buttom.end(), Height) - Buttom.begin();
+		total += N / 2 - index;
 
-		int index = lower_bound(Bottom.begin(), Bottom.end(), hight) - Bottom.begin();
-		cnt += N / 2 - index;
+		index = upper_bound(Top.begin(), Top.end(), H - Height) - Top.begin();
+		total += N / 2 - index;
 
-		index = upper_bound(Top.begin(), Top.end(), H - hight) - Top.begin();
-		cnt += N / 2 - index;
-
-		
-		if (answer > cnt) {
-			answer = cnt;
-			num = 1;
-		}else if (answer == cnt) num++;
+		if (answer > total) {
+			answer = total;
+			cnt = 1;
+		}
+		else if (answer == total) cnt++;
 	}
 
-	cout << answer << " " << num;
+	cout << answer << " " << cnt;
 
 	return 0;
 }
